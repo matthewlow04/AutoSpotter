5fc2bbacd9df30bdfe01a0951e13dda91aa32da8
Matthew Low <matthew7419@gmail.com>
2023-06-01 11:44:18 -0400

    Initial Commit

---
+++ AutoSpotter/AutoSpotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoSpotterApp.swift
+//  AutoSpotter
+//
+//  Created by Matthew Low on 2023-06-01.
+//
+
+import SwiftUI
+
+@main
+struct AutoSpotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
