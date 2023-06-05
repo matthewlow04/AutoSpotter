507cf1d26cf505b1ca45da5cf74d74fc083b9b0c
Matthew Low <matthew7419@gmail.com>
2023-06-05 18:31:03 -0400

    Added individual car view in garage

---
+++ AutoSpotter/VIews/CarProfileView.swift
@@ -0,0 +1,88 @@
+//
+//  CarProfileView.swift
+//  AutoSpotter
+//
+//  Created by Matthew Low on 2023-06-05.
+//
+
+import SwiftUI
+
+struct CarProfileView: View {
+    var currentCar: Car
+    var body: some View {
+        ScrollView{
+           
+            
+            VStack(alignment: .leading){
+                ZStack(alignment: .bottomTrailing) {
+                    Image("car")
+                        .resizable()
+                        .scaledToFit()
+                    
+                    favoriteIcon    
+                }
+                locationView
+                    .padding()
+                notesView
+                    .padding()
+      
+            }
+            Spacer(minLength: 200)
+            
+            favouriteButton
+                .buttonStyle(HomeButtonStyle())
+        }
+        .navigationTitle(currentCar.name)
+    }
+    
+    @ViewBuilder var favoriteIcon: some View{
+        Image(systemName: "heart.fill")
+            .foregroundColor(Color.white)
+            .padding()
+            .background(Color.red)
+            .clipShape(Circle())
+            .overlay{
+                Circle()
+                    .strokeBorder(Color.white, lineWidth: 2)
+            }
+            .offset(x: -10, y: -10)
+    }
+    
+    var favouriteButton: some View {
+        
+        Button("Add Favourite"){
+            
+        }
+    }
+    
+    var locationView: some View{
+        VStack(alignment: .leading){
+            Text("LOCATION")
+                .font(.title2)
+                .foregroundColor(.gray)
+                .fontWeight(.black)
+            
+            Text(currentCar.location)
+            
+
+        }
+    }
+    
+    var notesView: some View{
+        VStack(alignment: .leading){
+            Text("NOTES")
+                .font(.title2)
+                .foregroundColor(.gray)
+                .fontWeight(.black)
+            
+            Text(currentCar.notes)
+
+        }
+    }
+}
+
+struct CarProfileView_Previews: PreviewProvider {
+    static var previews: some View {
+        CarProfileView(currentCar: mockCar().mockCar)
+    }
+}
